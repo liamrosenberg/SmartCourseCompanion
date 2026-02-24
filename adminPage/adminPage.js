cd3eae2b8e39a7e86a37a3a79a028391fd1ec06b
chrisvogas <chrisvogas8@gmail.com>
2026-02-23 23:23:56 -0500

    Added working enable/disable button. added course creation redirect

---
+++ adminPage/adminPage.js
@@ -0,0 +1,10 @@
+function enableDisable(btn){
+                                    
+                                        if(btn.innerText ==='Enabled'){
+                                            btn.innerText='Disabled';
+                                            btn.style.backgroundColor = "#ff3838";
+                                        }else{
+                                            btn.innerText = "Enabled";
+                                            btn.style.backgroundColor = "#30d630";
+                                        }
+                                    }
