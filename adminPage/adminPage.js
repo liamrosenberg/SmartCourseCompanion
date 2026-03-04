6f6b97013162ad991ca72d37e1d2ba4f09c0f181
chrisvogas <chrisvogas8@gmail.com>
2026-03-04 17:53:36 -0500

    added working edit buttons and appropriate javascript

--- adminPage/adminPage.js
+++ adminPage/adminPage.js
@@ -8,3 +8,65 @@ function enableDisable(btn){
                                             btn.style.backgroundColor = "#30d630";
                                         }
                                     }
+
+
+    const editWeightingsBtn = document.getElementById('edit-weightings-btn');
+    const weightValues = document.querySelectorAll('.weight-value');
+
+    editWeightingsBtn.addEventListener('click', function() {
+        const isEditing = this.innerText === 'Save Weightings';
+
+        if (isEditing) {
+            // Switch back to normal mode (Save)
+            weightValues.forEach(span => {
+                span.contentEditable = "false";
+                span.style.border = "none";
+                span.style.padding = "0";
+                span.style.backgroundColor = "transparent";
+            });
+            this.innerText = 'Edit Weightings';
+            
+            
+        } else {
+            // Switch to Edit mode
+            weightValues.forEach(span => {
+                span.contentEditable = "true";
+                span.style.border = "1px solid #547eaa"; 
+                span.style.padding = "2px 5px";
+                span.style.backgroundColor = "#fff";
+                span.style.borderRadius = "4px";
+            });
+            this.innerText = 'Save Weightings';
+        }
+    });
+
+    // --- Edit Assessments Logic ---
+    const editAssessmentsBtn = document.getElementById('edit-assessments-btn');
+    const assessmentItems = document.querySelectorAll('.assessment-item');
+
+    editAssessmentsBtn.addEventListener('click', function() {
+        const isEditing = this.innerText === 'Save Assessments';
+
+        if (isEditing) {
+            
+            assessmentItems.forEach(item => {
+                item.contentEditable = "false";
+                item.style.border = "none";
+                item.style.padding = "0";
+                item.style.backgroundColor = "transparent";
+            });
+            this.innerText = 'Edit Assessments';
+        } else {
+            // Switch to Edit mode
+            assessmentItems.forEach(item => {
+                item.contentEditable = "true";
+                item.style.border = "1px solid #547eaa";
+                item.style.padding = "2px 5px";
+                item.style.backgroundColor = "#fff";
+                item.style.borderRadius = "4px";
+            });
+            this.innerText = 'Save Assessments';
+        }
+    });
+
+    
