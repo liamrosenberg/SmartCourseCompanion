394d5ca5101018ccb223bb93105aa5e4f77dc3d5
liamrosenberg <liam.rosenberg@gmail.com>
2026-03-23 13:12:21 -0400

    Created assessment, course and user object outlines

---
+++ backend/models/Assessment.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const assessmentSchema = new mongoose.Schema({
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    courseCode: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String },
+    dueDate: { type: Date },
+    earnedMarks: { type: Number, min: 0}, // Earned marks means like the actual grade received
+    totalMarks: { type: Number, min: 1}, // Total marks means like the highest possible grade
+    isCompleted: { type: Boolean, default: false },
+}, { timestamps: true });
+
+const Assessment = mongoose.model('Assessment', assessmentSchema);
---
+++ backend/models/Course.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    courseName: {type: String, required: true},
+    courseCode: {type: String, required: true, unique: true},
+    instructor: {type: String, required: true},
+    categories: [{ // For categorizing assessments
+        name: { type: String, required: true }, // Like midterm, final, project, etc.
+        weight: { type: Number, required: true } // Like 30% for midterm, 50% for final, etc.
+    }],
+    isActive: {type: Boolean, default: false},
+}, { timestamps: true });
+
+const Course = mongoose.model('Course', courseSchema);
